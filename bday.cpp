b2ad2254e2335c89d1c67a537e7fe20d2145a752
maya <maya.little.baby.2018@gmail.com>
2023-08-13 19:38:44 -0700

    add bday.cpp

---
+++ bday.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void Sing()
+{
+  cout << "Twinkle twinkle little star" << endl;
+  cout << "How I wonder where you are" << endl;
+  cout << "Up above the world so high" << endl;
+  cout << "Like a diamond in the sky" << endl;
+  cout << "Twinkle twinkle little star" << endl;
+  cout << "How I wonder where you are" << endl;
+  cout << endl;
+}
+
+int main()
+{
+  Sing();
+  return 0;
+}
+
+
+
+
